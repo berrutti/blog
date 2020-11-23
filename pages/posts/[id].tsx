8f7c73be6d6ea785e91155e20849249fa6367319
Matias Berrutti <berrutti@gmail.com>
2020-11-23 19:07:44 -0300

    Code Highlights
    - Add highlight.js
    - Add css import
    - Add useEffect and useRef hooks to handle highlights
    - Remove picture from posts
    - Update nextjs

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,9 +1,14 @@
+import { useEffect, useRef } from 'react';
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
 import Head from 'next/head'
 import Date from '../../components/date'
 import utilStyles from '../../styles/utils.module.css'
 import { GetStaticProps, GetStaticPaths } from 'next'
+import hljs from 'highlight.js/lib/core';
+import javascript from 'highlight.js/lib/languages/javascript';
+
+hljs.registerLanguage('javascript', javascript);
 
 export default function Post({
   postData
@@ -14,6 +19,17 @@ export default function Post({
     contentHtml: string
   }
 }) {
+  const highlightRef = useRef(null);
+
+  useEffect(() => {
+    if (highlightRef) {
+      const nodes = highlightRef.current.querySelectorAll('code');
+      nodes.forEach((node) => {
+        hljs.highlightBlock(node);
+      });
+    }
+  }, [])
+
   return (
     <Layout>
       <Head>
@@ -24,7 +40,7 @@ export default function Post({
         <div className={utilStyles.lightText}>
           <Date dateString={postData.date} />
         </div>
-        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+        <div ref={highlightRef} dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </article>
     </Layout>
   )
